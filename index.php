03efe161533c8e2f15a2049681dd15e3eef80879
serevinaash <serevinasherly02@gmail.com>
2023-10-09 08:24:54 +0700

    Pesan commit Anda di sini

--- index.php
+++ index.php
@@ -9,7 +9,9 @@
     <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+	<link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+	<link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+
     <link rel="stylesheet" href="css/animate.css">
     
     <link rel="stylesheet" href="css/owl.carousel.min.css">
